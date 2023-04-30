bcfee7dfaeb631be7290f2a4dea14760a9b836f9
Pongsapat_1022 <pongsapat.inta@kmutt.acth>
2023-04-30 21:54:36 +0700

    Week4_Homework

---
+++ 3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int numbernode = int.Parse(Console.ReadLine());
+
+        List<int> edges = new List<int>();
+        while (true)
+        {
+            int edge = int.Parse(Console.ReadLine());
+            if (edge >= numbernode || edge < 0)
+            {
+                break;
+            }
+            edges.Add(edge);
+        }
+
+        int sourcenode = int.Parse(Console.ReadLine());
+        int destinationnode = int.Parse(Console.ReadLine());
+
+        int[,] adjMatrix = new int[numbernode, numbernode];
+        for (int i = 0; i < edges.Count; i += 2)
+        {
+            int NodeA = edges[i];
+            int NodeB= edges[i + 1];
+            adjMatrix[NodeA, NodeB] = 1;
+        }
+
+        int[,] path = (int[,])adjMatrix.Clone();
+        for (int k = 0; k < numbernode; k++)
+        {
+            for (int i = 0; i < numbernode; i++)
+            {
+                for (int j = 0; j < numbernode; j++)
+                {
+                    path[i, j] = path[i, j] == 1 || (path[i, k] == 1 && path[k, j] == 1) ? 1 : 0;
+                }
+            }
+        }
+
+        if (path[sourcenode, destinationnode] == 1)
+        {
+            Console.WriteLine("Reachable");
+        }
+        else
+        {
+            Console.WriteLine("Unreachable");
+        }
+         }
+}   
---
+++ 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+
+        List<int> Nodenondi = new List<int>();
+        for (int i = 0; ; i++)
+        {
+            int Firstnode = int.Parse(Console.ReadLine());
+            if (Firstnode >= size || Firstnode < 0)
+            {
+                break;
+            }
+            int Secondnode = int.Parse(Console.ReadLine());
+            if (Secondnode >= size || Secondnode < 0)
+            {
+                break;
+            }
+            if (Firstnode == Secondnode)
+            {
+                break;
+            }
+            else
+            {
+                Nodenondi.Add(Firstnode);
+                Nodenondi.Add(Secondnode);
+            }
+        }
+
+        List<int> usedNodes = Nodenondi.Select(x => x % size).Distinct().ToList();
+        int countNodes = usedNodes.Count - 2;
+        Console.WriteLine("Output is : {0}", countNodes);
+    }
+}
